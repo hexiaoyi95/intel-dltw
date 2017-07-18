cdf469a6445a3399bdd719b80ca543fc5f63cb0e
He,Xiaoyi <xiaoyi.he@intel.com>
2017-07-19 06:44:28 +0800

    modify sys.path.append to sys.path.insert to overide PYTHONPATH

--- backends/__init__.py
+++ backends/__init__.py
@@ -11,7 +11,8 @@ def backends_factory(backend_info):
     return a backend class by target backend_info
     """
 
-    sys.path.append(os.path.expanduser(backend_info.python_path))
+    #sys.path.append(os.path.expanduser(backend_info.python_path))
+    sys.path.insert(1, os.path.expanduser(backend_info.python_path))
     model = importlib.import_module(backend_info.class_path.rsplit('.', 1)[0])
     backend_class = getattr(model, backend_info.class_path.rsplit('.', 1)[1])
 
